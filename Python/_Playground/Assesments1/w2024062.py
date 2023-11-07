4893dc0fa8773d606623d83acd3bd1117de2e7ef
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-11-07 12:47:49 +0000

    nan

--- Python/_Playground/Assesments1/w2024062.py
+++ Python/_Playground/Assesments1/w2024062.py
@@ -1,75 +1,73 @@
-progression_outcomes = {}
-progression_results = {}
+VALID_VALUES = [0, 20, 40, 60, 80, 100, 120]
+progression_rules = {}
+progression_summary = {}
 
 
 def generate_key(pass_credits, defer_credits, fail_credits):
     return f"{pass_credits:03d}{defer_credits:03d}{fail_credits:03d}"
 
 
-class ProgressionOutcome:
+class ProgressionRules:
     def __init__(self, pass_credits, defer_credits, fail_credits, message):
         self.pass_credits = pass_credits
         self.defer_credits = defer_credits
         self.fail_credits = fail_credits
         self.message = message
 
-    def generate_key(self):
-        return generate_key(self.pass_credits, self.defer_credits, self.fail_credits)
 
-
-def add_progression_outcome(pass_credits, defer_credits, fail_credits, message):
-    progression_outcomes[
+def add_progression_rule(pass_credits, defer_credits, fail_credits, message):
+    progression_rules[
         generate_key(pass_credits, defer_credits, fail_credits)
-    ] = ProgressionOutcome(pass_credits, defer_credits, fail_credits, message)
+    ] = ProgressionRules(pass_credits, defer_credits, fail_credits, message)
 
 
-def add_progression_outcomes():
-    add_progression_outcome(120, 0, 0, "Progress")
-    add_progression_outcome(100, 20, 0, "Progress (module trailer)")
-    add_progression_outcome(100, 0, 20, "Progress (module trailer)")
-    add_progression_outcome(80, 40, 0, "Do not Progress - module retriever")
-    add_progression_outcome(80, 20, 20, "Do not Progress - module retriever")
-    add_progression_outcome(80, 0, 40, "Do not Progress - module retriever")
-    add_progression_outcome(60, 60, 0, "Do not progress - module retriever")
-    add_progression_outcome(60, 40, 20, "Do not progress - module retriever")
-    add_progression_outcome(60, 20, 40, "Do not progress - module retriever")
-    add_progression_outcome(60, 0, 60, "Do not progress - module retriever")
-    add_progression_outcome(40, 80, 0, "Do not progress - module retriever")
-    add_progression_outcome(40, 60, 20, "Do not progress - module retriever")
-    add_progression_outcome(40, 40, 40, "Do not progress - module retriever")
-    add_progression_outcome(40, 20, 60, "Do not progress - module retriever")
-    add_progression_outcome(40, 0, 80, "Exclude")
-    add_progression_outcome(20, 100, 0, "Do not progress - module retriever")
-    add_progression_outcome(20, 80, 20, "Do not progress - module retriever")
-    add_progression_outcome(20, 60, 40, "Do not progress - module retriever")
-    add_progression_outcome(20, 40, 60, "Do not progress - module retriever")
-    add_progression_outcome(20, 20, 80, "Exclude")
-    add_progression_outcome(20, 0, 100, "Exclude")
-    add_progression_outcome(0, 120, 0, "Do not progress - module retriever")
-    add_progression_outcome(0, 100, 20, "Do not progress - module retriever")
-    add_progression_outcome(0, 80, 40, "Do not progress - module retriever")
-    add_progression_outcome(0, 60, 60, "Do not progress - module retriever")
-    add_progression_outcome(0, 40, 80, "Exclude")
-    add_progression_outcome(0, 20, 100, "Exclude")
-    add_progression_outcome(0, 0, 120, "Exclude")
+def add_progression_rules():
+    add_progression_rule(120, 0, 0, "Progress")
+    add_progression_rule(100, 20, 0, "Progress (module trailer)")
+    add_progression_rule(100, 0, 20, "Progress (module trailer)")
+    add_progression_rule(80, 40, 0, "Do not Progress - module retriever")
+    add_progression_rule(80, 20, 20, "Do not Progress - module retriever")
+    add_progression_rule(80, 0, 40, "Do not Progress - module retriever")
+    add_progression_rule(60, 60, 0, "Do not progress - module retriever")
+    add_progression_rule(60, 40, 20, "Do not progress - module retriever")
+    add_progression_rule(60, 20, 40, "Do not progress - module retriever")
+    add_progression_rule(60, 0, 60, "Do not progress - module retriever")
+    add_progression_rule(40, 80, 0, "Do not progress - module retriever")
+    add_progression_rule(40, 60, 20, "Do not progress - module retriever")
+    add_progression_rule(40, 40, 40, "Do not progress - module retriever")
+    add_progression_rule(40, 20, 60, "Do not progress - module retriever")
+    add_progression_rule(40, 0, 80, "Exclude")
+    add_progression_rule(20, 100, 0, "Do not progress - module retriever")
+    add_progression_rule(20, 80, 20, "Do not progress - module retriever")
+    add_progression_rule(20, 60, 40, "Do not progress - module retriever")
+    add_progression_rule(20, 40, 60, "Do not progress - module retriever")
+    add_progression_rule(20, 20, 80, "Exclude")
+    add_progression_rule(20, 0, 100, "Exclude")
+    add_progression_rule(0, 120, 0, "Do not progress - module retriever")
+    add_progression_rule(0, 100, 20, "Do not progress - module retriever")
+    add_progression_rule(0, 80, 40, "Do not progress - module retriever")
+    add_progression_rule(0, 60, 60, "Do not progress - module retriever")
+    add_progression_rule(0, 40, 80, "Exclude")
+    add_progression_rule(0, 20, 100, "Exclude")
+    add_progression_rule(0, 0, 120, "Exclude")
 
 
-def init_progression_results():
-    for key, value in progression_outcomes.items():
-        progression_results[value.message.upper()] = 0
+def init_progression_summary():
+    for key, value in progression_rules.items():
+        progression_summary[value.message.upper()] = 0
 
 
-def multiple_outcomes():
+def input_data():
     while True:
-        pass_credits = input_validation("pass")
-        defer_credits = input_validation("defer")
-        fail_credits = input_validation("fail")
+        pass_credits = input_credits_and_validate("pass")
+        defer_credits = input_credits_and_validate("defer")
+        fail_credits = input_credits_and_validate("fail")
 
         total = pass_credits + defer_credits + fail_credits
         if total == 120:
-            outcome_key = f"{pass_credits:03d}{defer_credits:03d}{fail_credits:03d}"
-            progression_results[progression_outcomes[outcome_key].message.upper()] += 1
-            print(progression_outcomes[outcome_key].message)
+            outcome_key = generate_key(pass_credits, defer_credits, fail_credits)
+            progression_summary[progression_rules[outcome_key].message.upper()] += 1
+            print(progression_rules[outcome_key].message)
             print()
             print("Would you like to enter another set of data?")
             more = input("Enter 'y' for yes or 'q' to quit and view results: ")
@@ -79,29 +77,28 @@ def multiple_outcomes():
             print("Total incorrect")
 
 
-def input_validation(verb):
+def input_credits_and_validate(verb):
     while True:
         value = input(f"Please enter your credits at {verb}: ")
         if value.isdigit():
             value = int(value)
-            valid_values = [0, 20, 40, 60, 80, 100, 120]
-            if value in valid_values:
+            if value in VALID_VALUES:
                 return value
             else:
-                print("Out of range")
+                print(f"Out of range, acceptable values are {VALID_VALUES}")
         else:
-            print("Integer required")
+            print(f"Integer required, acceptable values are {VALID_VALUES}")
 
 
 def show_histogram():
-    for key, value in progression_results.items():
+    for key, value in progression_summary.items():
         print(key, value)
 
 
 def main():
-    add_progression_outcomes()
-    init_progression_results()
-    multiple_outcomes()
+    add_progression_rules()
+    init_progression_summary()
+    input_data()
     show_histogram()
 
 
