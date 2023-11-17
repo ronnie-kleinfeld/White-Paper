df403d5dcc740c15d49955649c45a6f65f4eb9aa
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-11-17 14:07:12 +0000

    triangle

--- C#/Playground .NET 6.0/CoreConsoleApp/Program.cs
+++ C#/Playground .NET 6.0/CoreConsoleApp/Program.cs
@@ -6,7 +6,20 @@
         {
             Console.WriteLine("Hello, World!");
 
+            PrintTriangle();
             PrimitiveTypes.PT();
+        }
+
+        private static void PrintTriangle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
