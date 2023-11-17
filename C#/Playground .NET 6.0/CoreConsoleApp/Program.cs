c348b009445fa437a3b22025f7848bdd06b28971
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-11-17 11:13:20 +0000

    init, Ronnie is back

---
+++ C#/Playground .NET 6.0/CoreConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ronnie is back!");
+        }
+    }
+}
