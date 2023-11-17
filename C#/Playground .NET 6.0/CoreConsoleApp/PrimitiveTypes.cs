da8b499b13ad40442a1b2f4f191388af87a1187b
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-11-17 12:33:27 +0000

    Init, Ronnie is back1

---
+++ C#/Playground .NET 6.0/CoreConsoleApp/PrimitiveTypes.cs
@@ -0,0 +1,26 @@
+namespace CoreConsoleApp
+{
+    public class PrimitiveTypes
+    {
+        public static void PT()
+        {
+            Boolean b1 = true; // {true,false}
+
+            Byte byte1 = 0; // [0,255]
+            SByte sb = 0; // [-128,127]
+
+            Char c = 'a';
+
+            Int16 i1 = 1;
+            Int32 i2 = 2;
+            Int64 i3 = 2;
+
+            UInt16 i4 = 1;
+            UInt32 i5 = 2;
+            UInt64 i6 = 2;
+
+            Single s = 32.12f; // 32bit single precision floating point number
+            Double d = 32.12f; // 64bit double precision floating point number
+        }
+    }
+}
