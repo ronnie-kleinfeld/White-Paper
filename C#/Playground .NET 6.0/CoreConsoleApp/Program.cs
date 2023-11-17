50eaf742cc4651eacd1fbd8ad16e04808d1ba075
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-11-17 12:30:08 +0000

    Revert "nan"
    This reverts commit 1d2e0a568a07056c4c2c3a4520f924c70178789e.

---
+++ C#/Playground .NET 6.0/CoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            PrimitiveTypes.PT();
+        }
+    }
+}
