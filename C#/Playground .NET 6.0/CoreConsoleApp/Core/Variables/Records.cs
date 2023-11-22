47d2566c288b1fa54485cb9b561ad14f38692f99
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-22 12:42:24 +0000

    record

---
+++ C#/Playground .NET 6.0/CoreConsoleApp/Core/Variables/Records.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreConsoleApp.Core.Variables
+{
+    public record class CardsRecord(string Suit, string Rank);
+
+    public class Test
+    {
+        public Test()
+        {
+            var myCards = new CardsRecord("Heart", "Ace");
+            Console.WriteLine(myCards.Rank);
+        }
+    }
+}
