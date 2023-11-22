9cda3de0bdeb197d3a83ce9cdc544a63b7e03709
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-22 22:48:30 +0000

    nan

--- C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/Arrays.cs
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/Arrays.cs
@@ -1,12 +1,23 @@
-namespace CoreConsoleApp.Core.Variables
+namespace PlaygroundTest.Core.Variables
 {
-    public class Arrays
+    [TestClass]
+    public class ArrayTest
     {
-        public static void Init()
+        [TestMethod]
+        public void Init()
         {
             int[] numbers = new int[8];
+            Assert.AreEqual(8, numbers.Length);
+
             double[] taxRates = { 3.5, 5.8, 3.1 };
-            var letters = new Char[] { 'a', 'b', 'c' };
+            Assert.AreEqual(taxRates.Length, 3);
+
+            var letters = new Char[] { 'a', 'b', 'c', 'd' };
+            Assert.AreEqual(letters.Length, 4);
+            Assert.AreEqual('a', letters[0]);
+            Assert.AreEqual('b', letters[1]);
+            Assert.AreEqual('c', letters[2]);
+            Assert.AreEqual('d', letters[3]);
         }
     }
 }
