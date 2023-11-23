9978361dae50e5a324cf5f15df47219e0229ee56
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-23 12:24:21 +0000

    ValueVsReferenceTypeTest

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/CharTest.cs
@@ -0,0 +1,17 @@
+﻿namespace PlaygroundTest.Core.Variables
+{
+    [TestClass]
+    public class CharTest
+    {
+        [TestMethod]
+        public void Casr()
+        {
+            int a = 1;
+            char c = 'A';
+
+            Assert.AreEqual("66", $"{a + c}");
+            Assert.AreEqual("66", $"{c + a}");
+            Assert.AreEqual("B", $"{(char)(c + a)}");
+        }
+    }
+}
