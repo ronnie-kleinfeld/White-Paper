7dc6cf79bbf328702adeedcf36eac379d9886c2f
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-25 17:48:05 +0000

    nan

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/StringBuilderTest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace PlaygroundTest.Core.Variables
+{
+    [TestClass]
+    public class StringBuilderTest
+    {
+        [TestMethod]
+        public void Init()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Assert.AreEqual(0, sb.Length);
+        }
+    }
+}
