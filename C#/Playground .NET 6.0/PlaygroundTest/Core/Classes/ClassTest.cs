50a33db7dc2c36792d4b9116887c130f9adf99d2
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-27 12:39:49 +0000

    Struct

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Classes/ClassTest.cs
@@ -0,0 +1,38 @@
+﻿namespace PlaygroundTest.Core.Classes
+{
+    [TestClass]
+    public class ClassTest
+    {
+        [TestMethod]
+        public void Init()
+        {
+            Person person = new Person(1, "name");
+            Assert.IsNotNull(person);
+            Assert.AreEqual(1, person.Id);
+            Assert.AreEqual("name", person.Name);
+        }
+    }
+
+    public class Person
+    {
+        // methods
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // ctor
+        public Person()
+        {
+        }
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // methods
+        public string ToString()
+        {
+            return $"ID={Id}, Name={Name}";
+        }
+    }
+}
---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/StructTest.cs
@@ -0,0 +1,39 @@
+﻿namespace PlaygroundTest.Core.Variables
+{
+    [TestClass]
+    public class StructTest
+    {
+        [TestMethod]
+        public void Init()
+        {
+            Location location = new Location(1, 3);
+            Assert.IsNotNull(location);
+            Assert.AreEqual(1, location.Latitude);
+            Assert.AreEqual(3, location.Longitude);
+            location.Latitude = 5;
+            location.Longitude = 7;
+            Assert.IsNotNull(location);
+            Assert.AreEqual(5, location.Latitude);
+            Assert.AreEqual(7, location.Longitude);
+        }
+    }
+
+    public struct Location
+    {
+        // methods
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+
+        public Location(int latitude, int longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        // methods
+        public string ToString()
+        {
+            return $"Latitude={Latitude}, Longitude={Longitude}";
+        }
+    }
+}
