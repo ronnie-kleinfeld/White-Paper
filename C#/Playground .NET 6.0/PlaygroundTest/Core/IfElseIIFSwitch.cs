f127d5b0629822c00848e90bd0c98b26c21d40fe
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-27 16:46:44 +0000

    IfElseIIFSwitch

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/IfElseIIFSwitch.cs
@@ -0,0 +1,71 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PlaygroundTest.Core
+{
+    [TestClass]
+    public class IfElseIIFSwitch
+    {
+        [TestMethod]
+        public void Testif()
+        {
+            Assert.AreEqual(1, DoIf(1));
+            Assert.AreEqual(2, DoIf(2));
+            Assert.AreEqual(-1, DoIf(3));
+        }
+        public int DoIf(int input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+            else if (input == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        [TestMethod]
+        public void TestIIf()
+        {
+            Assert.AreEqual(true, DoIIf(1));
+            Assert.AreEqual(false, DoIIf(2));
+        }
+        public bool DoIIf(int input)
+        {
+            return input == 1 ? true : false;
+        }
+
+        [TestMethod]
+        public void TestSwitch()
+        {
+            Assert.AreEqual(1, DoSwitch(1));
+            Assert.AreEqual(23, DoSwitch(2));
+            Assert.AreEqual(23, DoSwitch(3));
+            Assert.AreEqual(-1, DoSwitch(4));
+        }
+        public int DoSwitch(int input)
+        {
+            int result;
+
+            switch (input)
+            {
+                case 1:
+                    result = 1;
+                    break;
+                case 2:
+                case 3:
+                    result = 23;
+                    break;
+                default:
+                    result = -1;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/EnumTest.cs
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Variables/EnumTest.cs
@@ -6,10 +6,22 @@
         [TestMethod]
         public void Init()
         {
+            GenderEnum gender = GenderEnum.Other;
+            Assert.AreEqual(GenderEnum.Other, gender);
+            Assert.AreEqual(3, (int)gender);
+
+            Assert.AreEqual(1, (int)GenderEnum.Male);
+            Assert.AreEqual(GenderEnum.Female, (GenderEnum)2);
+            Assert.AreEqual(GenderEnum.Female.ToString(), "Female");
+            Assert.AreEqual(GenderEnum.Other, Enum.Parse(typeof(GenderEnum), "Other"));
         }
     }
 
-    public enum GenderEnum : int // default enum value is int
+    /// <summary>
+    /// default enum value is int
+    /// first item value defaults to 0 and ++ each others
+    /// </summary>
+    public enum GenderEnum : int
     {
         Male = 1,
         Female = 2,
