e60c8f7965ceab5c28a5b205bcf95ee45da4645b
ronniekleinfeld <ronnie.kleinfeld@gmail>
2023-11-27 17:32:02 +0000

    nan

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/System/RandomTest.cs
@@ -0,0 +1,14 @@
+﻿namespace PlaygroundTest.Core.System
+{
+    [TestClass]
+    public class RandomTest
+    {
+        [TestMethod]
+        public void Init()
+        {
+            Random r = new Random();
+            int i = r.Next(0, 1);
+            Assert.IsTrue(i >= 0 && i <= 1);
+        }
+    }
+}
