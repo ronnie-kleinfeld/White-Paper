6c42031bb952d74f80793710c85b3e264916ed4a
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-12-13 21:25:20 +0000

    nan

--- Python/Software 1/_ICT/_dictionaries.py
+++ Python/Software 1/_ICT/_dictionaries.py
@@ -24,29 +24,38 @@ print(dic.values())
 ##############################################################################
 print("ITER")
 dic = dict(a=1, b=2, c=3, d=4)
-for key in dic.keys():
-    print(key, end=" ")
+print("Keys: ", end=" ")
+for item in dic.keys():
+    print(item, end=", ")
 print()
 
+print("dic: ", end=" ")
 for key in dic:
-    print(key, dic[key], end=" ")
+    print(key, dic[key], end=", ")
 print()
 
+print("Values: ", end=" ")
+for item in dic.values():
+    print(item, end=", ")
+print()
+
+print("Items: ", end=" ")
 for item in dic.items():
-    print(item, end=" ")
+    print(item, end=", ")
 print()
 
+print("Key, Value: ", end=" ")
 for key, value in dic.items():
-    print(key, value, end=" ")
+    print(key, value, end=", ")
 print()
 
-values = {x: x * 2 for x in range(5)}
+values = {x: x * 2 for x in range(5) if x % 2 == 0}
 print(values)
 
 print("DELETE")
-del dic["x"]
-print(dic)  # {'y': 2, 'z': 7}
+del dic["a"]
+print(dic)  # {'b': 2, 'c': 3, 'd': 4}
 dic["y"] = None
-print(dic)  # {'y': None, 'z': 7}
+print(dic)  # {'b': 2, 'c': 3, 'd': 4, 'y': None}
 dic.clear()
 print(dic)  # {}
