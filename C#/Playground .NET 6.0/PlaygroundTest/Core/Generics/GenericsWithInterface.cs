039afba3056acd1aa39270bae1545ac1d8ff1203
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-12-29 14:45:30 +0000

    MultipleGenericsSample

---
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Generics/GenericsWithInterface.cs
@@ -0,0 +1,56 @@
+﻿namespace PlaygroundTest.Core.Generics
+{
+    public class MultipleGenericsSample
+    {
+        public void Sample()
+        {
+            var c = new Customer
+            {
+                Id = 7,
+                FirstName = "customer",
+                LastName = "first",
+            };
+
+            var mapper = new CustomerToPersonMapper();
+            var p = c.Map<Person>(mapper);// mapper.Map(c);
+
+            Console.WriteLine(p.FirstName);
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public T Map<T>(IMapper<Customer, T> mapper)
+        {
+            return mapper.Map(this);
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public interface IMapper<S, T>
+    {
+        T Map(S source);
+    }
+    public class CustomerToPersonMapper : IMapper<Customer, Person>
+    {
+        public Person Map(Customer source)
+        {
+            return new Person
+            {
+                Id = source.Id,
+                FirstName = source.FirstName,
+                LastName = source.LastName
+            };
+        }
+    }
+}
--- C#/Playground .NET 6.0/PlaygroundTest/Core/Var/VarTest.cs
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Var/VarTest.cs
@@ -8,6 +8,11 @@
         {
             var p1 = new Employee();
             Assert.AreEqual("Employee Do1A", p1.Do1A());
+
+            var ip1 = new Employee();
+            IPerson ip2 = new Employee();
+            Person ip3 = new Employee();
+            Employee ip = new Employee();
         }
     }
 }
