3ff75db32da4e632e9909b68fc0f412bcd2a3c7b
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2023-12-31 19:54:02 +0000

    CallerMemberName

--- C#/Playground .NET 6.0/PlaygroundTest/Core/Attributes/AttributesTest.cs
+++ C#/Playground .NET 6.0/PlaygroundTest/Core/Attributes/AttributesTest.cs
@@ -1,4 +1,6 @@
-﻿namespace PlaygroundTest.Core.Attributes
+﻿using System.Runtime.CompilerServices;
+
+namespace PlaygroundTest.Core.Attributes
 {
     [TestClass]
     public class AttributesTest
@@ -6,7 +8,21 @@
         [TestMethod]
         public void Init()
         {
+            SomeMethod("Ronnie", 2);
             Assert.AreEqual(1, 1);
+        }
+
+        public void SomeMethod(string name, int id, [CallerArgumentExpression("name")] string arg = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = "")
+        {
+            Assert.AreEqual("Init", memberName);
+
+            Console.WriteLine(arg);
+            Console.WriteLine(filePath);
+            Console.WriteLine(lineNumber);
+            Console.WriteLine(memberName);
         }
     }
 }
