b4aa07d567ece6ba6f24487dbb5b05e4177baf20
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-01 19:12:03 +0000

    Playground .NET 7.0

---
+++ C#/Playground .NET 7.0/Playground .NET 7.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Playground_.NET_7._0
+{
+    public class TestClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .Net 7");
+
+            var e = new Emp();
+            e.Add(5, 6);
+            Console.WriteLine(e.Add('c', 'f'));
+        }
+    }
+
+
+    public class Emp
+    {
+        public T Add<T>(T left, T right) where T : INumber<T>
+        {
+            return left + right;
+        }
+    }
+}
