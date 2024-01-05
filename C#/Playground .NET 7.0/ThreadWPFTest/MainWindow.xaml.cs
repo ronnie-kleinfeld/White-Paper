c4f0863b9ed32cf339445fcfaba9382715646430
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-05 18:46:10 +0000

    UpdateUIThreadFromAnotherThread

---
+++ C#/Playground .NET 7.0/ThreadWPFTest/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace ThreadWPFTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(ChangeCompanyName);
+        }
+
+        private void ChangeCompanyName()
+        {
+            Thread.Sleep(2500);
+            UpdateUIThreadFromAnotherThread();
+        }
+
+        private void UpdateUIThreadFromAnotherThread()
+        {
+            //txtMessage.Text = "123"; excetion
+            //Dispatcher.Invoke(() => txtMessage.Text = "234");
+
+            //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() => txtMessage.Text = "345"));
+
+            //Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() => txtMessage.Text = "456"));
+        }
+    }
+}
