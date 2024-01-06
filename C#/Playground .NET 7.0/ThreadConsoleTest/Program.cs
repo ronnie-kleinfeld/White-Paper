b8db1cb43c6184bc01ef27903a48b956ad9da080
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-06 21:31:21 +0000

    nan

--- C#/Playground .NET 7.0/ThreadConsoleTest/Program.cs
+++ C#/Playground .NET 7.0/ThreadConsoleTest/Program.cs
@@ -8,22 +8,27 @@ namespace ThreadConsoleTest
     {
         static async Task Main(string[] args)
         {
-            _ThreadInlineAction.Start();
+            // Parallels
+            //ParallelforImage.Start();
+
+            // Synchronization
+            //Lock.Start();
+            //MutexLock.Start();
+            //SemaphoreSlimLock.Start();
+
+            // Tasks
+            //await BreakfastAsync.Cook(args);
+            //BreakfastSync.Cook(args);
+            //FunctionAsAsyncSample.Start();
+            //ProgressOfAsyncProcess.Start();
+            //await TAPSample.Start();
+            //TaskChaining.Start();
+
+            // Threads
+            //_ThreadInlineAction.Start();
             //ThreadAFunction1.Start();
             //ThreadAFunction2.Start();
             //ThreadSendDataCallback.Start();
-
-            //FunctionAsAsyncSample.Start();
-
-            //BreakfastSync.Cook(args);
-            //await BreakfastAsync.Cook(args);
-            //await TAPSample.Start();
-            //ProgressOfAsyncProcess.Start();
-            //Lock.Start();
-            //TaskChaining.Start();
-            //MutexLock.Start();
-            //SemaphoreSlimLock.Start();
-            //ParallelforImage.Start();
         }
     }
 }
