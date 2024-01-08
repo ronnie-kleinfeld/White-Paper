60d860478e17b041115e125eb0cf69cfd3471116
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-08 17:31:05 +0000

    nan

--- C#/Playground .NET 7.0/ThreadConsoleTest/Parallels/_Parallel.cs
+++ C#/Playground .NET 7.0/ThreadConsoleTest/Parallels/_Parallel.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ThreadConsoleTest.Parallels
+﻿namespace ThreadConsoleTest.Parallels
 {
     internal class _Parallel
     {
+        public static void Start()
+        {
+            Parallel.For(0, 100, (ix) => { Console.WriteLine(ix); });
+
+            var numbers = new List<int>();
+            for (int ix = 0; ix < 100; ix++)
+            {
+                numbers.Add(ix);
+            }
+            Parallel.ForEach<int>(numbers, (ix) => { });
+            Console.ReadKey();
+        }
     }
 }
