c552f545ae460e3ff63273286a44f4a0990623b6
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 15:58:31 +0000

    nan

--- C#/Playground .NET 4.8/EFCodeFirst/Program.cs
+++ C#/Playground .NET 4.8/EFCodeFirst/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace EFCodeFirst
+﻿using EFCodeFirst.Context;
+using System;
+using System.Linq;
+
+namespace EFCodeFirst
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new EFCodeFirstDBDBContext();
 
+            var course = dbContext.Courses.FirstOrDefault();
+            Console.WriteLine(course);
+            Console.ReadKey();
         }
     }
 }
