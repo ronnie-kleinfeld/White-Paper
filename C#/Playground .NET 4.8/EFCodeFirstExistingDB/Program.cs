04371d8205e5e8d14a6bc3a6f9250540798b00ea
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 17:51:12 +0000

    EF

--- C#/Playground .NET 4.8/EFCodeFirstExistingDB/Program.cs
+++ C#/Playground .NET 4.8/EFCodeFirstExistingDB/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace EFCodeFirstExistingDB
+﻿using EFCodeFirstExistingDB.Context;
+using System;
+using System.Linq;
+
+namespace EFCodeFirstExistingDB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new EFCodeFirstDBDBContext();
+
+            var course = dbContext.Courses.FirstOrDefault();
+            Console.WriteLine(course);
+            Console.ReadKey();
         }
     }
 }
