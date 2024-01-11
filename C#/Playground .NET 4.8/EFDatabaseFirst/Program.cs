2ae3fdc0f8e73c325caaa69980b18fba692d004f
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 13:55:55 +0000

    Database-first

--- C#/Playground .NET 4.8/EFDatabaseFirst/Program.cs
+++ C#/Playground .NET 4.8/EFDatabaseFirst/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace EFDatabaseFirst
+﻿using System;
+using System.Linq;
+
+namespace EFDatabaseFirst
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new EFDatabaseFirstDBDBContext();
+            var courses = dbContext.GetCourses();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Title);
+            }
+
+            var count = dbContext.Courses.Count<Course>();
+            Console.WriteLine(count);
+
+            Console.ReadKey();
         }
     }
 }
