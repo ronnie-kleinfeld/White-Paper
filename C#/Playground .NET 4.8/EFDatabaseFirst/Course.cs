82e3612228ea7bcb27a06fd471fe588af93da7c0
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 12:42:07 +0000

    nan

--- C#/Playground .NET 4.8/EFDatabaseFirst/Course.cs
+++ C#/Playground .NET 4.8/EFDatabaseFirst/Course.cs
@@ -12,10 +12,10 @@ namespace EFDatabaseFirst
     using System;
     using System.Collections.Generic;
     
-    public partial class Cours
+    public partial class Course
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Cours()
+        public Course()
         {
             this.CourseSections = new HashSet<CourseSection>();
             this.Tags = new HashSet<Tag>();
