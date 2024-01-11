263056d6cccfe0e69e4c938e1c917e5a65384f8f
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 11:46:18 +0000

    EF

---
+++ EntityFramework OLD/EFSample/DLEF/Handlers/AuthorRepository.cs
@@ -0,0 +1,32 @@
+﻿using DLEF.Context;
+using DLEF.Handlers.Base.Data;
+using DLEF.Handlers.Base.Map;
+using DLEF.Handlers.Base.Repository;
+
+namespace DLEF.Handlers
+{
+    public interface IAuthorRepository : IRepository<AuthorData>
+    {
+    }
+    public class AuthorRepository : Repository<AuthorData>, IAuthorRepository
+    {
+        public AuthorRepository(DLContext context) : base(context)
+        {
+        }
+    }
+
+    internal class AuthorIDMap : BaseIDMap<AuthorData>
+    {
+        public AuthorIDMap() : base("Authors")
+        {
+            Property(v => v.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+
+    public class AuthorData : BaseIDData
+    {
+        public string Name { get; set; }
+    }
+}
