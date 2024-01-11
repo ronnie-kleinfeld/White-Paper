c2a37a32b5b0870edf00cb6e2fc904c7a1a53333
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-11 11:52:16 +0000

    nan

---
+++ C#/Playground .NET 4.8/Playground .NET 4.8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Playground.NET_4._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello old friend");
+            Console.ReadKey();
+        }
+    }
+}
