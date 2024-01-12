efd3dbfebfec65d5f061575874f57aea2bfd587e
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-12 13:39:42 +0000

    EF Core - EFCodeFirstSQLServer

---
+++ C#/Playground .NET 7.0/EFCodeFirstSQLServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCodeFirstSQLServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
