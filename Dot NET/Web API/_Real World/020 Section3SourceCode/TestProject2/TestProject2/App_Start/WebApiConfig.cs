3cd66496397d41a61e98de1cc17541cce1b00860
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-22 19:45:12 +0000

    nan

--- Dot NET/Web API/_Real World/020 Section3SourceCode/TestProject2/TestProject2/App_Start/WebApiConfig.cs
+++ Dot NET/Web API/_Real World/020 Section3SourceCode/TestProject2/TestProject2/App_Start/WebApiConfig.cs
@@ -1,22 +1,18 @@
-﻿using DeLoachAero.WebApi;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Http.Routing;
 using TestProject2.Models;
 
-namespace TestProject2
-{
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
+namespace TestProject2 {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
             // Web API configuration and services
 
             // Web API routes
             //config.MapHttpAttributeRoutes();
             var constraintResolver = new DefaultInlineConstraintResolver();
-            constraintResolver.ConstraintMap.Add("enum", typeof(EnumerationConstraint));
+            //constraintResolver.ConstraintMap.Add("enum", typeof(EnumerationConstraint));
             constraintResolver.ConstraintMap.Add("base64", typeof(Base64Constraint));
             config.MapHttpAttributeRoutes(constraintResolver);
 
