ad5688a718510b1048e29840911cfb1428b5c993
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-26 19:48:26 +0000

    VidlyWithMosh4.8

---
+++ Dot NET/ASP.Net/VidlyWithMosh4.8/VidlyWithMosh4.8/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VidlyWithMosh4._8 {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Dot NET/ASP.Net/VidlyWithMosh4.8/VidlyWithMosh4.8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyWithMosh4._8.MvcApplication" Language="C#" %>
