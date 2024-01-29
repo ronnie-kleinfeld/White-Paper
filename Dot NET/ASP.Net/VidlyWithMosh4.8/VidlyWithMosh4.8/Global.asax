c626d81b2ad39921c676b438c370dea22e62cc2c
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-29 17:21:16 +0000

    finish vidly asp.net with mosh

--- Dot NET/ASP.Net/VidlyWithMosh4.8/VidlyWithMosh4.8/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyWithMosh4._8.MvcApplication" Language="C#" %>
--- Dot NET/ASP.Net/VidlyWithMosh4.8/VidlyWithMosh4.8/Models/Movie.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace VidlyWithMosh4._8.Models {
-    public class Movie {
-        public int ID { get; set; }
-        public string Name { get; set; }
-    }
-}
