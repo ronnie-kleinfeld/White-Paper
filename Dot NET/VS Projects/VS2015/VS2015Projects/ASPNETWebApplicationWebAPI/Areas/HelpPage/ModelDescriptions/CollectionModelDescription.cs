de82c88378490df24142b41b099ad720cc90c869
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-01-31 15:33:17 +0000

    VS2015Projects

---
+++ Dot NET/VS Projects/VS2015/VS2015Projects/ASPNETWebApplicationWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ASPNETWebApplicationWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Dot NET/VS Projects/VS2015/VS2015Projects/ASPNETWebApplicationWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETWebApplicationWebForms.Startup))]
+namespace ASPNETWebApplicationWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
