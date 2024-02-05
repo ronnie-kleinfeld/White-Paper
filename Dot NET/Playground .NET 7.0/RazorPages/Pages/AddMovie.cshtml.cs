d9147527e88ebb10c0d72af34468b7c6dc9a1241
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-05 17:10:52 +0000

    data binding

--- Dot NET/Playground .NET 7.0/RazorPages/Pages/AddMovie.cshtml.cs
+++ Dot NET/Playground .NET 7.0/RazorPages/Pages/AddMovie.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPages.Pages
-{
-    public class AddMovieModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace RazorPages.Pages {
+    [BindProperties]
+    public class AddMovieModel : PageModel {
+        //[BindProperty]
+        public string Title { get; set; }
+        //[BindProperty]
+        public string Rate { get; set; }
+        //[BindProperty]
+        public string Description { get; set; }
+
+        public void OnGet() {
+            Title = "Welcome";
+        }
+
+        public IActionResult OnPost() {
+            string value = $"{Title} - {Rate} - {Description}";
+
+            return Page();
+            return Redirect("Moviews");
         }
     }
 }
--- Dot NET/Playground .NET 7.0/RazorPages/Pages/Movie.cshtml.cs
+++ Dot NET/Playground .NET 7.0/RazorPages/Pages/Movie.cshtml.cs
@@ -1,12 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPages.Pages
-{
-    public class MovieModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
+namespace RazorPages.Pages {
+    public class MovieModel : PageModel {
     }
 }
