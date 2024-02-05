35531dadc2216243ff699635490c5b15d7b98571
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-05 16:53:13 +0000

    movies

--- Dot NET/Playground .NET 7.0/RazorPages/Pages/Movies.cshtml.cs
+++ Dot NET/Playground .NET 7.0/RazorPages/Pages/Movies.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Data.Model;
 
-namespace RazorPages.Pages
-{
-    public class MoviesModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace RazorPages.Pages {
+    public class MoviesModel : PageModel {
+        public List<Movie> Movies { get; set; }
+
+        public void OnGet() {
+            Movies = new List<Movie>() {
+                new Movie() {
+                    Id = 1,
+                    Title = "Test",
+                    Rate = 100,
+                    Description = "Test"
+                },
+                new Movie() {
+                    Id = 2,
+                    Title = "Test 2",
+                    Rate = 99,
+                    Description = "Test 2"
+                }
+            };
         }
     }
 }
