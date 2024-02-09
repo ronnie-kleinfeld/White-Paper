4707f3d102c444578ae77e3f21e4281b9f4fcbaa
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-09 14:41:13 +0000

    nan

---
+++ Dot NET/Playground .NET 7.0/ASPNETCoreWebAPI/Controllers/ROUTEController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ROUTEController : Controller {
+        // /ROUTE/METHOD_ROUTE
+        [HttpGet]
+        [Route("METHOD_ROUTE")]
+        public IEnumerable<int> METHOD() {
+            return Enumerable.Range(1, 5);
+        }
+    }
+}
