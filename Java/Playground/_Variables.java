ca9300fad907f7b8832497b7ab83d900f28edf17
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-13 22:39:58 +0000

    nan

--- Java/Playground/_Variables.java
+++ Java/Playground/_Variables.java
@@ -18,12 +18,17 @@ public class _Variables {
         String[] arrstr = new String[3];
         char[] arrc = new char[3];
         Boolean[] arrb = new Boolean[3];
-        _Rect[] arrr = new _Rect[3];
+        _Box[] arrr = new _Box[3];
 
         System.out.println(Arrays.toString(arri));
         System.out.println(Arrays.toString(arrstr));
         System.out.println(Arrays.toString(arrc));
         System.out.println(Arrays.toString(arrb));
         System.out.println(Arrays.toString(arrr));
+
+        char c1 = 'a';
+        System.out.println(c1); // a
+        System.out.println((int) c1); // 97
+        System.out.println((char) 98);// b
     }
 }
