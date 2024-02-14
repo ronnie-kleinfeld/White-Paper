f620d6668f21728dcb315c8255ee0585f1adb7a4
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-14 11:20:24 +0000

    nan

--- Java/Playground/_Variables.java
+++ Java/Playground/_Variables.java
@@ -3,27 +3,35 @@ import java.util.Arrays;
 public class _Variables {
     public static void main(String[] args) {
         int i = 0;
-        double d = 0;
-        String str = "";
-        char c = ' ';
-        Boolean b = true; // false;
-
         System.out.println(i);
+
+        double d = 0;
         System.out.println(d);
+
+        String str = "";
         System.out.println(str);
+
+        char c = ' ';
         System.out.println(c);
+
+        Boolean b = true; // false;
         System.out.println(b);
 
-        int[] arri = new int[3];
-        String[] arrstr = new String[3];
-        char[] arrc = new char[3];
-        Boolean[] arrb = new Boolean[3];
-        _Box[] arrr = new _Box[3];
+        System.out.println();
 
+        int[] arri = new int[3];
         System.out.println(Arrays.toString(arri));
+
+        String[] arrstr = new String[3];
         System.out.println(Arrays.toString(arrstr));
+
+        char[] arrc = new char[3];
         System.out.println(Arrays.toString(arrc));
+
+        Boolean[] arrb = new Boolean[3];
         System.out.println(Arrays.toString(arrb));
+
+        _Box[] arrr = new _Box[3];
         System.out.println(Arrays.toString(arrr));
 
         char c1 = 'a';
