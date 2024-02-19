7b3feae4967b4695c4297e3630ed7dabc454ae59
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-19 18:03:58 +0000

    nan

---
+++ Java/Software 2/W2024062_PlaneManagement/src/main/java/com/mycompany/w2024062_planemanagement/Plane.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.w2024062_planemanagement;
+
+/**
+ *
+ * @author ronniekleinfeld
+ */
+public class Plane {
+
+    private Seat[][] seats;
+
+    public Plane() {
+        seats = new Seat[4][14];
+    }
+}
---
+++ Java/Software 2/W2024062_PlaneManagement/src/main/java/com/mycompany/w2024062_planemanagement/PriceEnum.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.w2024062_planemanagement;
+
+/**
+ *
+ * @author ronniekleinfeld
+ */
+public enum PriceEnum {
+    firstClass,
+    business,
+    tourist
+}
