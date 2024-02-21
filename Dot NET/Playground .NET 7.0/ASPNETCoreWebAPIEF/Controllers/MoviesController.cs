8be0af9bf7cf9a9cb1612eaa776e73afe8a5542d
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-02-21 15:57:19 +0000

    web api

---
+++ Dot NET/Playground .NET 7.0/ASPNETCoreWebAPIEF/Controllers/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using ASPNETCoreWebAPIEF.Data;
+using ASPNETCoreWebAPIEF.Data.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPIEF.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase {
+        private ApplicationDBContext dbContext;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public MoviesController(ApplicationDBContext dbContext, ILogger<WeatherForecastController> logger) {
+            this.dbContext = dbContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Movie> Index() {
+            return dbContext.Movies.ToList();
+        }
+
+        [HttpPost]
+        public Movie Post(string title, string description, int rate) {
+            Movie movie = new Movie() {
+                Title = title,
+                Description = description,
+                Rate = rate
+            };
+
+            dbContext.Movies.Add(movie);
+            dbContext.SaveChanges();
+
+            return movie;
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, string title, string description, int rate) {
+            Movie? movie = dbContext.Movies.FirstOrDefault<Movie>(m => m.Id == id);
+
+            if (movie == null) {
+                return NotFound();
+            } else {
+                movie.Title = title;
+                movie.Description = description;
+                movie.Rate = rate;
+
+                dbContext.Movies.Add(movie);
+                dbContext.SaveChanges();
+
+                return Ok(movie);
+            };
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id) {
+            Movie? movie = dbContext.Movies.FirstOrDefault<Movie>(m => m.Id == id);
+
+            if (movie == null) {
+                return NotFound();
+            } else {
+                dbContext.Movies.Remove(movie);
+                dbContext.SaveChanges();
+
+                return Ok();
+            }
+        }
+    }
+}
