36e4ab512d610eeb1de4c40ce7dc20448806d5d0
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-04-05 22:44:57 +0100

    signal R working example

--- Dot NET/Playground .NET 8.0/SignalRChatRoomWebApplication/ChatHub.cs
+++ Dot NET/Playground .NET 8.0/SignalRChatRoomWebApplication/ChatHub.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChatRoomWebApplication {
     public class ChatHub : Hub {
+        // [Authorize(Roles = "Admin")]
         public async Task SendMessage(string user, string message) {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message + " 014290985");
+        }
+
+        public async override Task OnConnectedAsync() {
+            await Clients.All.SendAsync("ReceiveMessage", Context.UserIdentifier, "OnConnectedAsync");
+        }
+
+        public async override Task OnDisconnectedAsync(Exception? exception) {
+            await Clients.All.SendAsync("ReceiveMessage", Context.UserIdentifier, "OnDisconnectedAsync");
         }
     }
 }
