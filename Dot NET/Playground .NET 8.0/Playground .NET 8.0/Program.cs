ff1659cbc70079b13ae1d3f48fc21bdb7c76c2ee
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-04-05 21:27:54 +0100

    Playground .NET 8.0

---
+++ Dot NET/Playground .NET 8.0/Playground .NET 8.0/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Playground_.NET_8._0 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World! Ronnie");
+        }
+    }
+}
