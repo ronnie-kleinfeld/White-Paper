0d18f052d0f8d3f607bc5b1734ff8e2ab5551b5c
Ronnie Kleinfeld <ronnie.k@comtecglobal.com>
2024-09-23 16:17:52 +0300

    code first

---
+++ Dot NET/Entity Framework/EntityFramework Core/WhatFirst/CodeFirst/Context/BloggingContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirst.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.Context {
+    public class BloggingContext : DbContext {
+        public DbSet<Blog> Blogs {
+            get; set;
+        }
+        public DbSet<Post> Posts {
+            get; set;
+        }
+
+        public BloggingContext() {
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCoreDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+    }
+}
