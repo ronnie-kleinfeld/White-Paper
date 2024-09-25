2860820fd7cfff5aa679d6d6bd269519ab6093e4
Ronnie Kleinfeld <ronnie.k@comtecglobal.com>
2024-09-25 09:54:49 +0300

    EFCore2

---
+++ Dot NET/Entity Framework/EFCore - Tabula Rasa/EFCore2/DLEFCore2/Context/DLEFCoreContext.cs
@@ -0,0 +1,30 @@
+﻿using DLEFCore2.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DLEFCore2.Context {
+    public class DLEFCoreContext : DbContext {
+        // members
+        public DbSet<UserModel> Users {
+            get; set;
+        }
+
+        // class
+        public DLEFCoreContext() {
+            //this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // events
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCoreDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            //new NameMap().Configure(modelBuilder.Entity<NameModel>());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Dot NET/Entity Framework/EFCore - Tabula Rasa/EFCore2/DLEFCore2/Repository/UserModel.cs
@@ -0,0 +1,27 @@
+﻿namespace DLEFCore2.Repository {
+    public class UserModel {
+        public int Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        }
+
+        public bool IsActive {
+            get; set;
+        }
+
+        public DateTime CreatedDateTime {
+            get; set;
+        }
+
+        public int Age {
+            get; set;
+        }
+
+        public decimal value {
+            get; set;
+        }
+    }
+}
