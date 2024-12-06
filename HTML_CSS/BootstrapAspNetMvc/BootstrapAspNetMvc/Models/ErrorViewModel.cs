0061b2a966d0ef92654da1751cd03e9c342581e7
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-12-06 17:30:45 +0000

    BootstrapAspNetMvc

---
+++ HTML_CSS/BootstrapAspNetMvc/BootstrapAspNetMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootstrapAspNetMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
